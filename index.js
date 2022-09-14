a1645c65d190bed69c4d0050b4001ce6d417bcfd
Sonia Chalal <contact@soniawebdesigner.fr>
2022-09-14 15:26:31 +0200

    Création de l'api rest avec framework express node.js et sqlite + ejs pour la vue de l'api avec bootstrap

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const sqlite3 = require("sqlite3").verbose();
-const dbname='univers.db';
-let db = new sqlite3.Database(dbname, err => {
-    if(err)
-    throw err
-    console.log("Base de donnée initialisé dans le fichier : " + dbname);
+// Importation du module Express
+const express = require("express");
+// Instancie le serveur
+const app = express();
+// Le serveur est démarré et attend les requêtes arrivant sur le port 3000.
+// La fonction callback sert a afficher un message informatif lorsque le serveur est prêt à recevoir des requêtes
+app.listen(3000, () => {
+    console.log("Serveur démarré (http://localhost:3000/) !");
 });
-db.run('CREATE TABLE IF NOT EXISTS planetes(id INTEGER PRIMARY KEY, name VARCHAR(25), image VARCHAR(255), created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)');
+// La fonction pour répondre(res) aux requêtes(req) GET pointant sur la racine du site.
+// La méthode app.get répond aux requêtes passées en paramètre c'est à dire "/" la racine du site.
+app.get("/", (req, res) => {
+    res.send("Bonjour bienvenu dans l'api planete...");
+});
