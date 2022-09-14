5c32520e6853a6527bd206d4be27ff970a38b33e
Sonia Chalal <contact@soniawebdesigner.fr>
2022-09-14 16:39:04 +0200

    Création de la base de données univers.db et de sa table planetes

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ app.listen(7500, () => {
 // La méthode app.get répond aux requêtes passées en paramètre c'est à dire "/" la racine du site.
 app.get("/", (req, res) => {
     res.send("Bienvenue dans l'api rest des planetes");
+})
+
+const sqlite3 = require("sqlite3").verbose();
+const dbname = 'univers.db';
+let db = new sqlite3.Database(dbname, err => {
+    if (err)
+        throw err
+    console.log("Base de donnée initialisé dans le fichier : " + dbname);
 });
+db.run('CREATE TABLE IF NOT EXISTS planetes(id INTEGER PRIMARY KEY, nom VARCHAR(25), image VARCHAR(255), created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)');
