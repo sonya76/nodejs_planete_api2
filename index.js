2d704ecffa5f8bc888c7a990a24b053961beb50d
Sonia Chalal <contact@soniawebdesigner.fr>
2022-09-14 18:09:45 +0200

    Inserer dans la table planetes les planetes Venus,Terre,Mercure,Mars avec une image(url) pour chaque planete.

--- index.js
+++ index.js
@@ -11,4 +11,23 @@ app.listen(7500, () => {
 // La méthode app.get répond aux requêtes passées en paramètre c'est à dire "/" la racine du site.
 app.get("/", (req, res) => {
     res.send("Bienvenue dans l'api rest des planetes");
+})
+
+const sqlite3 = require("sqlite3").verbose();
+const dbname = 'univers.db';
+let db = new sqlite3.Database(dbname, err => {
+    if (err)
+        throw err
+    console.log("Base de donnée initialisé dans le fichier : " + dbname);
+});
+//db.run('CREATE TABLE IF NOT EXISTS planetes(id INTEGER PRIMARY KEY, nom VARCHAR(25), image VARCHAR(255), created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)');
+db.run('INSERT INTO planetes(nom,image) values (?,?)', ["Vénus", "./planetes/venus.jpeg"]);
+db.run('INSERT INTO planetes(nom,image) values (?,?)', ["Terre", "./planetes/terre.jpeg"]);
+db.run('INSERT INTO planetes(nom,image) values (?,?)', ["Mercure", "./planetes/mercure.jpeg"]);
+db.run('INSERT INTO planetes(nom,image) values (?,?)', ["Mars", "./planetes/mars.jpeg"]);
+// // db.get : Affiche la 1ere ligne
+// db.All : Affiche tout les enregistrements on ne peut afficher une ligne ou un champs demandé
+db.all('SELECT * FROM planetes', (err, data) => {
+    if (err) throw err;
+    console.log(data);
 });
