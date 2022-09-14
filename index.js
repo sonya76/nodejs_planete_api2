c3382892d6f6dd14f1ad8029b37e7edafbc19999
Sonia Chalal <contact@soniawebdesigner.fr>
2022-09-14 15:41:10 +0200

    Mise à jour du port 7500 avec le message de bienvenu demandé

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const express = require("express");
 // Instancie le serveur
 const app = express();
-// Le serveur est démarré et attend les requêtes arrivant sur le port 3000.
+// Le serveur est démarré et attend les requêtes arrivant sur le port 7500.
 // La fonction callback sert a afficher un message informatif lorsque le serveur est prêt à recevoir des requêtes
-app.listen(3000, () => {
-    console.log("Serveur démarré (http://localhost:3000/) !");
+app.listen(7500, () => {
+    console.log("Serveur démarré (http://localhost:7500/) !");
 });
 // La fonction pour répondre(res) aux requêtes(req) GET pointant sur la racine du site.
 // La méthode app.get répond aux requêtes passées en paramètre c'est à dire "/" la racine du site.
 app.get("/", (req, res) => {
-    res.send("Bonjour bienvenu dans l'api planete...");
+    res.send("Bienvenue dans l'api rest des planetes");
 });
