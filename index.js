9b44ba34642be74df5068fa638cacd783a9bed8b
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-09-15 11:07:52 +0200

    La route /planetes envoye la liste de toutes les planetes au format json (id,nom,image,created_at)

--- index.js
+++ index.js
@@ -20,14 +20,18 @@ let db = new sqlite3.Database(dbname, err => {
         throw err
     console.log("Base de donnée initialisé dans le fichier : " + dbname);
 });
+
+
 //db.run('CREATE TABLE IF NOT EXISTS planetes(id INTEGER PRIMARY KEY, nom VARCHAR(25), image VARCHAR(255), created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)');
 db.run('INSERT INTO planetes(nom,image) values (?,?)', ["Vénus", "./planetes/venus.jpeg"]);
 db.run('INSERT INTO planetes(nom,image) values (?,?)', ["Terre", "./planetes/terre.jpeg"]);
 db.run('INSERT INTO planetes(nom,image) values (?,?)', ["Mercure", "./planetes/mercure.jpeg"]);
 db.run('INSERT INTO planetes(nom,image) values (?,?)', ["Mars", "./planetes/mars.jpeg"]);
 // // db.get : Affiche la 1ere ligne
+app.get("/planetes", (req, res) => {
+    res.send("Listes des planetes");
 // db.All : Affiche tout les enregistrements on ne peut afficher une ligne ou un champs demandé
 db.all('SELECT * FROM planetes', (err, data) => {
     if (err) throw err;
     console.log(data);
-});
+});})
